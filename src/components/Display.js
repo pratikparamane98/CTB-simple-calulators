459c32aef07c042b935fb417772d019d89f78937
pratikparamane98 <56255793+pratikparamane98@users.noreply.github.com>
2021-06-10 20:00:15 +0530

    created button component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,33 +1,28 @@
 import React,{useState} from 'react'
-import { Button } from '@material-ui/core';
+import Button from '../components/button.js'
 
-import useStyles from './styles1'
+
 
 
 
 const Display = () => {
-    const classes = useStyles();
     var evaluate
     // arra operators
 
     const [input, setinput] = useState("")
-    const [show, setShow] = useState(false)//will toggle the scientific button
-    // const [first,setFirst] = useState(true)//will avoid the first input from being an operator
+    
 
 
-    const handleClick = (event) =>{
-        
-            evaluate = input.concat(event.target.name) 
-            // if(evaluate.length>evaluate.length-1){setinput(evaluate) }
-            setinput(evaluate)
-                
-                        
-                  
-         
+    const handleClick = (data) =>{
+            console.log("data")
+            evaluate=input.concat(data)
+            setinput(evaluate)            
         }
        
 
-    const operatorClick = (event) =>{
+    
+
+    const operatorClick = (data2) =>{
         
         if(input==""){
             setinput("")
@@ -47,7 +42,7 @@ const Display = () => {
               setinput(input.replace("/","/"));
              }
              else{
-               setinput(eval(input).toString() + (event.target.name));
+               setinput(eval(input).toString() + (data2));
              }
         
 
@@ -55,22 +50,25 @@ const Display = () => {
 
 
 
-    const clear=(e)=>{
+
+    const clear=()=>{
         setinput('');
         // setFirst(true)
 
     }
 
-    const backspace = (e) =>{
+    const backspace = () =>{
         setinput(input.slice(0,input.length - 1))
     }
 
-    const calculate = (e) =>{
+    const calculate = () =>{
         if(input==""){
             setinput("")
         }else{
         setinput(eval(input).toString())
         }
+        // var e = setinput(eval(input))
+        // console.log(e)
     }
 
     const squareRoot = (e) =>{
@@ -94,45 +92,14 @@ const Display = () => {
             <h1 className="h1">Calculator</h1>
             <br></br>
             <input type="text" className = "input" value={input} autoComplete="off" placeholder="0"></input>
-            <div>
-                <div>
-                    <button className="button" onClick={handleClick} name = "1">1</button>
-                    <button className="button" onClick={handleClick} name = "2">2</button>
-                    <button className="button" onClick={handleClick} name = "3">3</button>
-                    <button className="button" onClick={operatorClick} name = "+">+</button>
-                </div>   
-                <div> 
-                    <button className="button" onClick={handleClick} name = "4">4</button>
-                    <button className="button" onClick={handleClick} name = "5">5</button>
-                    <button className="button" onClick={handleClick} name = "6">6</button>
-                    <button className="button" onClick={operatorClick} name = "-">-</button>
-                </div>
-                <div>
-                    <button className="button" onClick={handleClick} name = "7">7</button>
-                    <button className="button" onClick={handleClick} name = "8">8</button>
-                    <button className="button" onClick={handleClick} name = "9">9</button>
-                    <button className="button" onClick={operatorClick} name = "*">*</button>
-                </div>
-                <div>
-                    <button className="button" onClick={handleClick} name = "0">0</button>
-                    <button className="button" onClick={calculate} >=</button>
-                    <button className="button" onClick={backspace} >C</button>
-                    <button className="button" onClick={operatorClick} name = "/">/</button>
-                </div>
-                <div>
-                    <Button className={classes.root}  onClick={clear} >clear</Button>
-                    <Button className={classes.root} onClick={() => setShow(!show)}>Scientific Mode</Button>
-                </div>
-        
-               {show?
-                    <div>
-                        <button className="button03" onClick= { SignClick } >Sign</button>
-                        <button className="button03" onClick= { square } >Square</button>
-                        <button className="button03" onClick= { squareRoot }>Square Root</button>
-                    </div>
-                :null}
-
-            </div>  
+            <Button allowed = {handleClick}
+                    equal = {calculate}
+                    backsp={backspace}
+                    clearr={clear}
+                    signCl={SignClick}
+                    sqr={square}
+                    sqrt={squareRoot}
+                    operator = {operatorClick}/>
 
             </div>  
         </div>
---
+++ src/components/button.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Button } from '@material-ui/core';
+// import UseStyles from './styles1'
+
+
+
+export const button = (props) => {
+    // const classes = UseStyles();
+    var data=['0','1','2','3','4','5','6','7','8','9']
+    var data2=['+','-','*','/']
+
+    // const [show, setShow] = useState(false)//will toggle the scientific button
+
+
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <button className="button" onClick={()=>props.allowed(data[1])}>1</button>
+                    <button className="button" onClick={()=>props.allowed(data[2])}>2</button>
+                    <button className="button" onClick={()=>props.allowed(data[3])}>3</button> 
+                     <button className="button" onClick={()=>props.operator(data2[0])}>+</button>
+                </div>   
+                <div> 
+                    <button className="button" onClick={()=>props.allowed(data[4])}>4</button>
+                    <button className="button" onClick={()=>props.allowed(data[5])}>5</button>
+                    <button className="button" onClick={()=>props.allowed(data[6])}>6</button>
+                    <button className="button" onClick={()=>props.operator(data2[1])}>-</button>
+                </div>
+                <div>
+                    <button className="button" onClick={()=>props.allowed(data[7])}>7</button>
+                    <button className="button" onClick={()=>props.allowed(data[8])}>8</button>
+                    <button className="button" onClick={()=>props.allowed(data[9])}>9</button>
+                    <button className="button" onClick={()=>props.operator(data[2])}>*</button>
+                </div>
+                <div>
+                    <button className="button" onClick={()=>props.allowed(data[0])}>0</button>
+                    <button className="button" onClick={()=>props.equal()}>=</button>
+                    <button className="button" onClick={()=>props.backsp()} >C</button>
+                    <button className="button" onClick={()=>props.operator(data2[3])}>/</button>
+                </div>
+                 {/* <div>
+                    <Button className={classes.root}  onClick={()=>props.clearr()}>clear</Button>
+                    <Button className={classes.root} onClick={() => setShow(!show)}>Scientific Mode</Button>
+                </div> */}
+                <div>
+                        <button className="button03"onClick={()=>props.signCl()}>Sign</button>
+                        <button className="button03" onClick={()=>props.sqr()} >Square</button>
+                        <button className="button03" onClick={()=>props.sqrt()}>Square Root</button>
+                        <button className="button03"  onClick={()=>props.clearr()}>clear</button>
+                </div>
+        
+               {/* {show?
+                    <div>
+                        <button className="button03"onClick={()=>props.signCl()}>Sign</button>
+                        <button className="button03" onClick={()=>props.sqr()} >Square</button>
+                        <button className="button03" onClick={()=>props.sqrt()}>Square Root</button>
+                    </div>
+                :null}  */}
+
+            </div>  
+        </div>
+    )
+}
+
+export default button;
